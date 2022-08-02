4605964f0340895e505823c16e21785614ad0175
rokoblox <minecraftroko123@gmail.com>
2022-08-02 18:22:59 +0200

    Created the native rust library

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub extern fn hello_world() {
+    println!("Hello from rust!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
